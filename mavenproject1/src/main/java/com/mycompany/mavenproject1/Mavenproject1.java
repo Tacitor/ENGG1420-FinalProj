649aa13a217d014008c57910b8a2c3dc06623c2f
Fluff-E <116575016+Fluff-E@users.noreply.github.com>
2023-03-11 20:24:30 -0500

    Create Maven Project. Test Laserfiche API

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,85 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+import com.laserfiche.api.client.model.AccessKey;
+import com.laserfiche.repository.api.RepositoryApiClient;
+import com.laserfiche.repository.api.RepositoryApiClientImpl;
+import com.laserfiche.repository.api.clients.impl.model.Entry;
+import com.laserfiche.repository.api.clients.impl.model.ODataValueContextOfIListOfEntry;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ *
+ * @author keric
+ */
+public class Mavenproject1 {
+
+     public static void main(String[] args) {
+        String servicePrincipalKey = "z6N-YywpamJ5Dr227qKb";
+        String accessKeyBase64 = "ewoJImN1c3RvbWVySWQiOiAiMTQwMTM1OTIzOCIsCgkiY2xpZW50SWQiOiAiY2M4NWRlZGUtMGIzZS00ZGIyLTkyNzYtOWY5N2EzMGY2ZWZlIiwKCSJkb21haW4iOiAibGFzZXJmaWNoZS5jYSIsCgkiandrIjogewoJCSJrdHkiOiAiRUMiLAoJCSJjcnYiOiAiUC0yNTYiLAoJCSJ1c2UiOiAic2lnIiwKCQkia2lkIjogInh0UFBLcl8zbGVSa1dFQi1QaU9yNkFDMzJETjRRdDd0VmI4amE4VHNwRTQiLAoJCSJ4IjogImtsZl9IOWgxMTN6dmRHcjZ0Rk5MRC1wYk15YWJWUjZzTk5Fb2l1d2NmSTgiLAoJCSJ5IjogInVWcUR4NlRSV0JBaWZTbXJ6NG9uX2NXeEZlTDVPc29xM2V6U0ZQbWtkRk0iLAoJCSJkIjogInJfMHZXYnB6Q2Z2YUYzYXZQeERhNzdKTzItUkRrSWctRnpHWENrbFRNQzgiLAoJCSJpYXQiOiAxNjc3Mjk3NTA4Cgl9Cn0=";
+		String repositoryId = "r-0001d410ba56";
+        AccessKey accessKey = AccessKey.createFromBase64EncodedAccessKey(accessKeyBase64);
+
+        RepositoryApiClient client = RepositoryApiClientImpl.createFromAccessKey(
+                servicePrincipalKey, accessKey);
+
+        // Get information about the ROOT entry, i.e. entry with ID=1
+        int rootEntryId = 1;
+        Entry entry = client.getEntriesClient()
+                .getEntry(repositoryId, rootEntryId, null).join();
+
+        System.out.println(
+                String.format("Entry ID: %d, Name: %s, EntryType: %s, FullPath: %s",
+                        entry.getId(), entry.getName(), entry.getEntryType(), entry.getFullPath()));
+
+        // Get information about the child entries of the Root entry
+        ODataValueContextOfIListOfEntry result = client
+                .getEntriesClient()
+                .getEntryListing(repositoryId, rootEntryId, true, null, null, null, null, null, "name", null, null, null).join();
+        List<Entry> entries = result.getValue();
+        for (Entry childEntry : entries) {
+            System.out.println(
+                    String.format("Child Entry ID: %d, Name: %s, EntryType: %s, FullPath: %s",
+                            childEntry.getId(), childEntry.getName(), childEntry.getEntryType(), childEntry.getFullPath()));
+        }
+
+        // Download an antry 
+        int entryIdToDownload = 25;
+        final String FILE_NAME = "DownloadedFile.txt";
+        Consumer<InputStream> consumer = inputStream -> {
+            File exportedFile = new File(FILE_NAME);
+            try (FileOutputStream outputStream = new FileOutputStream(exportedFile)) {
+                byte[] buffer = new byte[1024];
+                while (true) {
+                    int length = inputStream.read(buffer);
+                    if (length == -1) {
+                        break;
+                    }
+                    outputStream.write(buffer, 0, length);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        client.getEntriesClient()
+                .exportDocument(repositoryId, entryIdToDownload, null, consumer)
+                .join();
+
+        client.close();
+    }
+}
