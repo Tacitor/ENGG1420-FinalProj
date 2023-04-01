609ddb7cd511b3631a75f9ad19e85a880d6794f1
AlecMcBurney <alecmcburney@gmail.com>
2023-04-01 19:07:08 -0400

    Created the function of the process method.

--- FileProcessor/src/main/java/elements/filters/LengthFilter.java
+++ FileProcessor/src/main/java/elements/filters/LengthFilter.java
@@ -5,7 +5,11 @@
  */
 package elements.filters;
 
+import com.group7.FileProcessor.Util;
+import com.group7.FileProcessor.entries.Entry;
+import com.group7.FileProcessor.entries.FolderDoesNotContainTextException;
 import elements.ProcessingElement;
+import java.util.ArrayList;
 
 /**
  *
@@ -19,14 +23,78 @@ public class LengthFilter extends ProcessingElement {
 
     /**
      * Primary default constructor.
+     * @param operator
+     * @param length
      */
-    public LengthFilter() {
+    public LengthFilter(String operator, Long length) {
         super();
+        setOperator(operator);
+        setLength(length);
     }
 
     @Override
     public void process() {
-        throw new UnsupportedOperationException("Not supported yet.");
+
+        // Make a copy of the input of entries
+        ArrayList<Entry> inputCopy = this.getInputEntries();
+        ArrayList<Entry> input = new ArrayList<>();
+        for (int i = 0; i < inputCopy.size(); i++) {
+            input.add(inputCopy.get(i));
+        }
+        // Ouput of entries
+        ArrayList<Entry> output = new ArrayList<>();
+
+        for (int i = 0; i < input.size(); i++) {
+
+            if (Util.isFile(input.get(i).getAddress()) == 1) {
+                try {
+                    String content = input.get(i).getContents();
+                    switch (operator) {
+                        case "EQ" -> {
+                            if (content.length() == getLength()) {
+                                output.add(input.get(i));
+                            }
+                        }
+
+                        case "NEQ" -> {
+                            if (content.length() != getLength()) {
+                                output.add(input.get(i));
+                            }
+                        }
+
+                        case "GT" -> {
+                            if (content.length() > getLength()) {
+                                output.add(input.get(i));
+                            }
+                        }
+
+                        case "GTE" -> {
+                            if (content.length() >= getLength()) {
+                                output.add(input.get(i));
+                            }
+                        }
+
+                        case "LT" -> {
+                            if (content.length() < getLength()) {
+                                output.add(input.get(i));
+                            }
+                        }
+
+                        case "LTE" -> {
+                            if (content.length() <= getLength()) {
+                                output.add(input.get(i));
+                            }
+                        }
+
+                        default ->
+                            System.out.println("Invalid comparison operator.");
+                    }
+                } catch (FolderDoesNotContainTextException ex) {
+                    System.out.println("Error in LengthFilter.java\n" + ex);
+                }
+            }
+        }
+        setOutputEntries(output);
     }
 
     public long getLength() {
