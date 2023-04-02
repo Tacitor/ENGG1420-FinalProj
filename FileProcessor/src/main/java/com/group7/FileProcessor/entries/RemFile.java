3833a67e63b1de8935bcb1d249ebd0d5db216212
shale <shalevmatan421@gmail.com>
2023-04-02 12:32:41 -0400

    implimented remote entries(folders are a work in progress)

--- FileProcessor/src/main/java/com/group7/FileProcessor/entries/RemFile.java
+++ FileProcessor/src/main/java/com/group7/FileProcessor/entries/RemFile.java
@@ -4,8 +4,17 @@
  */
 package com.group7.FileProcessor.entries;
 
+import com.laserfiche.api.client.model.AccessKey;
+import com.laserfiche.repository.api.RepositoryApiClient;
+import com.laserfiche.repository.api.RepositoryApiClientImpl;
+import com.laserfiche.repository.api.clients.impl.model.Entry;
+import com.laserfiche.repository.api.clients.impl.model.ODataValueContextOfIListOfEntry;
 import java.io.File;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.function.Consumer;
 import java.io.IOException;
 
 /**
@@ -14,31 +23,66 @@ import java.io.IOException;
  */
 public class RemFile extends LocFile{
     
-    String accessKey;
+    int entryId;
     String repositoryId;
+    static String servicePrincipalKey = "z6N-YywpamJ5Dr227qKb";
+    static String accessKeyBase64 = "ewoJImN1c3RvbWVySWQiOiAiMTQwMTM1OTIzOCIsCgkiY2xpZW50SWQiOiAiY2M4NWRlZGUtMGIzZS00ZGIyLTkyNzYtOWY5N2EzMGY2ZWZlIiwKCSJkb21haW4iOiAibGFzZXJmaWNoZS5jYSIsCgkiandrIjogewoJCSJrdHkiOiAiRUMiLAoJCSJjcnYiOiAiUC0yNTYiLAoJCSJ1c2UiOiAic2lnIiwKCQkia2lkIjogInh0UFBLcl8zbGVSa1dFQi1QaU9yNkFDMzJETjRRdDd0VmI4amE4VHNwRTQiLAoJCSJ4IjogImtsZl9IOWgxMTN6dmRHcjZ0Rk5MRC1wYk15YWJWUjZzTk5Fb2l1d2NmSTgiLAoJCSJ5IjogInVWcUR4NlRSV0JBaWZTbXJ6NG9uX2NXeEZlTDVPc29xM2V6U0ZQbWtkRk0iLAoJCSJkIjogInJfMHZXYnB6Q2Z2YUYzYXZQeERhNzdKTzItUkRrSWctRnpHWENrbFRNQzgiLAoJCSJpYXQiOiAxNjc3Mjk3NTA4Cgl9Cn0=";
+    static AccessKey accessKey = AccessKey.createFromBase64EncodedAccessKey(accessKeyBase64);
     static int downloadnum=0;
     
-    public RemFile(String accessKey,String repositoryId) throws IOException{
+    public RemFile(int entryid,String repositoryId) throws IOException{
+
+        RepositoryApiClient client = RepositoryApiClientImpl.createFromAccessKey(
+                servicePrincipalKey, accessKey);
+        String name = client.getEntriesClient().getEntry(repositoryId, entryid, null).join().getName();
+        
+        
         File fold = new File("C:\\ENG1420Group7FileProccessor");
         boolean dir = true;
         if (!fold.exists()){
             dir = fold.mkdir();
         }
-        setAddress(fold.getAbsolutePath()+"\\filedownload"+downloadnum+".txt");
-        downloadnum++;
-        File newFile = new File(getAddress());
-        newFile.createNewFile();
-        this.accessKey = accessKey;
+        setAddress(fold.getAbsolutePath()+"\\"+name+".txt");
+        
+        Consumer<InputStream> consumer = inputStream -> {
+            File exportedFile = new File(getAddress());
+            try (FileOutputStream outputStream = new FileOutputStream(exportedFile)) {
+                byte[] buffer = new byte[1024];
+                while (true) {
+                    int length = inputStream.read(buffer);
+                    if (length == -1) {
+                        break;
+                    }
+                    outputStream.write(buffer, 0, length);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        
+        client.getEntriesClient()
+                .exportDocument(repositoryId, entryid, null, consumer)
+                .join();
+
+        client.close();
+        updateContents();
+        this.entryId = entryid;
         this.repositoryId = repositoryId;
     }
     
 
-    public String getAccessKey() {
-        return accessKey;
+    public int getAccessKey() {
+        return entryId;
     }
 
-    public void setAccessKey(String accessKey) {
-        this.accessKey = accessKey;
+    public void setEntrId(int entryid) {
+        this.entryId = entryid;
     }
 
     public String getRepositoryId() {
