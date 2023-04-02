a0343b55802f34a150ff1678dc6935eecc756ec6
AlecMcBurney <alecmcburney@gmail.com>
2023-04-02 14:54:18 -0400

    Added comments

--- FileProcessor/src/main/java/elements/filters/NameFilter.java
+++ FileProcessor/src/main/java/elements/filters/NameFilter.java
@@ -40,58 +40,81 @@ public class NameFilter extends ProcessingElement {
         ArrayList<Entry> output = new ArrayList<>();
 
         for (int i = 0; i < input.size(); i++) {
-            String name = getName(input.get(i).getAddress());
+            String name = getName(input.get(i).getAddress()); // Finds the name of the entry
             if (contains(key, name)) {
-                output.add(input.get(i));
+                output.add(input.get(i)); // Adds the entry to the ouput if the name contains the key
             }
         }
 
         setOutputEntries(output);
     }
-
+    
+    /**
+     * Accessor for the var Key
+     * @return 
+     */
     public String getKey() {
         return key;
     }
-
+    
+    /**
+     * Mutator for the var Key
+     * @param key 
+     */
     public void setKey(String key) {
         this.key = key;
     }
-
+     /**
+      * A method to find the name of the entry within the address
+      * 
+      * @param address
+      * @return 
+      */
     private String getName(String address) {
         String name = "";
-        int i = address.length() - 1;
-        // Find start of file or folder name
-        int nameLen = 0;
-        char ch = address.charAt(0);
+        int i = address.length() - 1; // Final index of the address
+        int nameLen = 0; 
+        char ch = address.charAt(0); // Make sure the while loop will start
         i--;
-        while (ch != 92) {
+        while (ch != 92) { // Get the length of the name of the entry
             nameLen++;
             ch = address.charAt(i);
             i--;
         }
+        // Put the name of the address togther in a string
         for (int j = address.length() - nameLen; j < address.length(); j++) {
             name += address.charAt(i);
         }
         return name;
     }
-
+    /**
+     * A method that checks for a String within a String
+     * 
+     * @param check
+     * @param content
+     * @return 
+     */
     private boolean contains(String check, String content) {
-        boolean found = false;
+        
+        boolean found = false; // Has check been found
         int contentLen = content.length();
         int checkLen = check.length();
         int similarity;
-        while (found == false) {
+        
+        while (found == false) { // Ends when check is found
             for (int i = 0; i < contentLen; i++) {
                 int j = 0;
-                if (content.charAt(i) == check.charAt(j)) {
+                if (content.charAt(i) == check.charAt(j)) { // Checks for first char similarity between both strings
                     similarity = 0;
+                    
+                    // Loop through both strings to check for continuing similarites
                     while (j < checkLen && i < contentLen && content.charAt(i) == check.charAt(j)) {
                         similarity++;
                         i++;
                         j++;
                     }
                     if (similarity == checkLen) {
-                        found = true;
+                        found = true; // Check has been found
                     }
                 }
             }
