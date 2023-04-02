5abaf437bad1e6e272ae4d948037a63af1caf3ea
AlecMcBurney <alecmcburney@gmail.com>
2023-04-02 01:12:36 -0400

    Added contains method from NameFilter. Made process with minor change from name filter.

--- FileProcessor/src/main/java/elements/filters/ContentFilter.java
+++ FileProcessor/src/main/java/elements/filters/ContentFilter.java
@@ -5,7 +5,12 @@
  */
 package elements.filters;
 
+import com.group7.FileProcessor.entries.Entry;
+import com.group7.FileProcessor.entries.FolderDoesNotContainTextException;
 import elements.ProcessingElement;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -25,7 +30,26 @@ public class ContentFilter extends ProcessingElement {
 
     @Override
     public void process() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        // Make a copy of the input of entries
+        ArrayList<Entry> inputCopy = this.getInputEntries();
+        ArrayList<Entry> input = new ArrayList<>();
+        for (int i = 0; i < inputCopy.size(); i++) {
+            input.add(inputCopy.get(i));
+        }
+        // Ouput of entries
+        ArrayList<Entry> output = new ArrayList<>();
+        
+        for (int i = 0; i < input.size(); i++) {
+            try {
+                if (contains(key, input.get(i).getContents())) {
+                    output.add(input.get(i));
+                }
+            } catch (FolderDoesNotContainTextException ex) {
+                System.out.println("Error in ContentFilter.java\n" + ex);
+            }
+        }
+        
+        setOutputEntries(output);
     }
 
     public String getKey() {
@@ -34,6 +58,30 @@ public class ContentFilter extends ProcessingElement {
 
     public void setKey(String key) {
         this.key = key;
+    }
+    
+    private boolean contains(String check, String content) {
+        boolean found = false;
+        int contentLen = content.length();
+        int checkLen = check.length();
+        int similarity;
+        while (found == false) {
+            for (int i = 0; i < contentLen; i++) {
+                int j = 0;
+                if (content.charAt(i) == check.charAt(j)) {
+                    similarity = 0;
+                    while (j < checkLen && i < contentLen && content.charAt(i) == check.charAt(j)) {
+                        similarity++;
+                        i++;
+                        j++;
+                    }
+                    if (similarity == checkLen) {
+                        found = true;
+                    }
+                }
+            }
+        }
+        return found;
     }
 
 }
