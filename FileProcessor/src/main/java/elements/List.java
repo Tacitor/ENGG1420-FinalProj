875968e487d2428b3cb3e659976829b5216c165e
emjvhart <116666578+emjvhart@users.noreply.github.com>
2023-04-02 18:13:33 -0400

    implemented list function :D

--- FileProcessor/src/main/java/elements/List.java
+++ FileProcessor/src/main/java/elements/List.java
@@ -3,12 +3,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package elements;
-
+import java.util.ArrayList;
+import com.group7.FileProcessor.entries.Entry;
 import com.group7.FileProcessor.Util;
+import com.group7.FileProcessor.entries.LocFolder;
 
 /**
  *
- * @author keric
+ * @author emj.vhart
  */
 public class List extends ProcessingElement {
 
@@ -24,14 +26,39 @@ public class List extends ProcessingElement {
 
     @Override
     public void process() {
-        throw new UnsupportedOperationException("Not supported yet.");
+       int untilMax = 0;//creating counter
+       boolean found = false;
+       ArrayList<Entry> entryList = this.getInputEntries();//gets entries
+       ArrayList<Entry> output = new ArrayList();//creates output entry arraylist
+       for (Entry e : entryList) {
+        String[] addresses = ((LocFolder) e).getSubFiles();//gets directories
+        for (String s : addresses) {
+            if (e.getAddress().equals(s)) {//compares addresses and entries
+               found = true; 
+            }
+        }
+        
+        if (found == true)
+        {
+        output.add(e);
+        untilMax++;
+            if(untilMax == max)//stops loop if max has been hit
+            {
+                break;
+            }
+        }
     }
-
-    public int getMax() {
+       
+       
+    }
+    
+    public int getMax()
+    {
         return max;
     }
 
-    public void setMax(int max) {
+    public void setMax(int max)
+    {
         this.max = Util.getIntGreaterThan1(max);
     }
 
